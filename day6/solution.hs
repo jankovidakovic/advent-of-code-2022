c8e1cd69ffe5cf1ac95642fdaf1846edba2f9ef9
jvidakovic <janko.vidakovic@gmail.com>
2022-12-17 23:02:52 +0100

    Improve solution

--- day6/solution.hs
+++ day6/solution.hs
@@ -1,15 +1,18 @@
-import Data.List (findIndices, nub)
+import Data.List (findIndices, nub, tails)
 
 slidingWindow :: Int -> [a] -> [[a]]
 slidingWindow n xs
   | length (take n xs) < n = []
   | otherwise = take n xs : slidingWindow n (tail xs)
 
-isMarker :: (Eq a) => Int -> [a] -> Bool
-isMarker n = (== n) . length . nub
+betterSlidingWindow :: Int -> [a] -> [[a]]
+betterSlidingWindow n = takeWhile ((== n) . length) . map (take n) . tails
+
+isMarker :: (Eq a) => [a] -> Bool
+isMarker xs = length (nub xs) == length xs
 
 firstMarker :: (Eq a) => Int -> [a] -> Int
-firstMarker n = (+ n) . head . findIndices (isMarker n) . slidingWindow n
+firstMarker n = (+ n) . head . findIndices isMarker . slidingWindow n
 
 main = do
   input <- readFile "input.txt"
